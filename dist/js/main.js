1bcc946072afc447dde860b2fa5af45d027d7bfb
Sim <simon.papanicolaou@uk.rsagroup.com>
2016-05-02 00:37:06 +0100

    fixed carousel and added news section markup and styling

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,40 +3,43 @@ $(document).ready(function(){
 	/* SLICK carousel */
 
 	$('.slider-container').slick({
-	  dots: true,
-	  infinite: false,
-	  speed: 300,
-	  dots: true,
-	  slide: '.slide-panel',
-	  rows: 2,
-	  slidesPerRow: 2,
-	  slidesToShow: 4,
-	  responsive: [
-	    {
-	      breakpoint: 1024,
-	      settings: {
-	      	slidesToShow: 4,
-	      	rows: 2,
-	      	slidesPerRow: 2
-	      }
-	    },
-	    {
-	      breakpoint: 600,
-	      settings: {
-	        slidesToShow: 4,
-	      	rows: 2,
-	      	slidesPerRow: 2
-	      }
-	    },
-	    {
-	      breakpoint: 480,
-	      settings: {
-	        slidesToShow: 4,
-	      	rows: 2,
-	      	slidesPerRow: 2
-	      }
-	    }
-	  ]
+
+		cssEase: 'ease',
+		dots: true,
+		prevArrow: '.carousel-prev',
+		nextArrow: '.carousel-next',
+		infinite: false,
+		speed: 300,
+		rows: 2,
+		slidesPerRow: 2
+
+		// responsive: [
+		//   {
+		//     breakpoint: 1024,
+		//     settings: {
+		//     	slidesToShow: 2,
+		//     	rows: 2,
+		//     	slidesPerRow: 2
+		//     }
+		//   },
+		//   {
+		//     breakpoint: 600,
+		//     settings: {
+		//       slidesToShow: 1,
+		//     	rows: 1,
+		//     	slidesPerRow: 2
+		//     }
+		//   },
+		//   {
+		//     breakpoint: 480,
+		//     settings: {
+		//       slidesToShow: 1,
+		//     	rows: 1,
+		//     	slidesPerRow: 1
+		//     }
+		//   }
+		// ]
+
 	});
 
 });
